a9fd8b08603a6abe4e15cdbc901cd5bf9a1dba3a
ahiginio <ahiginio1210@gmail.com>
2021-07-20 13:30:38 -0300

    checkout arreglo

---
+++ src/environments/firebaseconfig.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+apiKey: "AIzaSyAF4ON6ql_sMgpOJ17ZF9obkLiIqLLiiEw",
+    authDomain: "efectivo-digital.firebaseapp.com",
+    projectId: "efectivo-digital",
+    storageBucket: "efectivo-digital.appspot.com",
+    messagingSenderId: "394225840157",
+    appId: "1:394225840157:web:794a1a265e4fa98ddb96f4",
+    measurementId: "G-6FKRVD53VQ"
+}
